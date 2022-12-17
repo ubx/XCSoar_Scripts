b928fc0568f076c2dcf435864c22f42c0b868c70
Andreas Lüthi <andreas.luethi@gmx.net>
2022-12-17 20:15:24 +0100

    Add support for brightness adjustment with the two buttons (UP, DOWN).

---
+++ kobo/init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+### Note: the file below must NOT resist in scrips directory. Otherwise, we are not
+###       able to Export USB storage!!
+/usr/set_brightness.sh & 
---
+++ usr/set_brightness.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+device='/dev/input/event3'
+
+key_f23='*EV_KEY KEY_F23 0x00000001*'
+key_f24='*EV_KEY KEY_F24 0x00000001*'
+
+evdev-dump "$device" | while read line; do
+  case $line in
+  $key_f23)
+    value=$(cat /sys/class/backlight/mxc_msp430.0/brightness)
+    if [[ $value -lt 100 ]]; then
+      value=$((value + 10))
+      echo $value >/sys/class/backlight/mxc_msp430.0/brightness
+    fi
+    ;;
+  $key_f24)
+    value=$(cat /sys/class/backlight/mxc_msp430.0/brightness)
+    if [[ $value -ge 10 ]]; then
+      value=$((value - 10))
+      echo $value >/sys/class/backlight/mxc_msp430.0/brightness
+    fi
+    ;;
+  esac
+done
