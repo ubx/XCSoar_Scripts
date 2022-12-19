4c3a8a0f1e1ec2839e6f4a72772fafe7514eed52
Andreas Lüthi <andreas.luethi@gmx.net>
2022-12-19 16:28:58 +0100

    Use bash function.

--- usr/set_brightness.sh
+++ usr/set_brightness.sh
@@ -5,20 +5,21 @@ device='/dev/input/event3'
 key_f23='*EV_KEY KEY_F23 0x00000001*'
 key_f24='*EV_KEY KEY_F24 0x00000001*'
 
+get_br() { value=$(cat /sys/class/backlight/mxc_msp430.0/brightness); }
+set_br() { echo "$1" >/sys/class/backlight/mxc_msp430.0/brightness; }
+
 evdev-dump "$device" | while read line; do
   case $line in
   $key_f23)
-    value=$(cat /sys/class/backlight/mxc_msp430.0/brightness)
+    get_br
     if [ $value -lt 100 ]; then
-      value=$((value + 10))
-      echo $value >/sys/class/backlight/mxc_msp430.0/brightness
+      set_br $((value + 10))
     fi
     ;;
   $key_f24)
-    value=$(cat /sys/class/backlight/mxc_msp430.0/brightness)
+    get_br
     if [ $value -ge 10 ]; then
-      value=$((value - 10))
-      echo $value >/sys/class/backlight/mxc_msp430.0/brightness
+      set_br $((value - 10))
     fi
     ;;
   esac
