267824801b08ec0868278200da8201592b220226
Andreas Lüthi <andreas.luethi@gmx.net>
2023-01-22 15:54:46 +0100

    Detect Kobo models Libra 2 and Clara 2E.

--- kobo/scripts/sub/connect.sh
+++ kobo/scripts/sub/connect.sh
@@ -8,7 +8,15 @@ done
 /bin/dbus-daemon --system &
 
 insmod /drivers/mx6sll-ntx/wifi/sdio_bt_pwr.ko
-/sbin/rtk_hciattach -n -s 115200 ttymxc1 rtk_h5 &
+insmod /drivers/mx6sll-ntx/wifi/sdio_bt_pwr.ko
+
+model=`dd if=/dev/mmcblk0 bs=8 count=1 skip=64`
+if [ `expr substr $model 1 7` = SN-N418 ] ; then # Libra 2
+   /sbin/rtk_hciattach -n -s 115200 ttymxc1 rtk_h5 &
+elif [ `expr substr $model 1 7` = SN-N506 ] ; then # Clara 2E
+  /sbin/hciattach -n ttymxc1 any 1500000 flow -t 20 &
+fi
+
 hciconfig hci0 up
 
 sleep 3
