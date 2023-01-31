e3026598a40297f47f675cc3ee822dca51f231a2
Andreas Lüthi <andreas.luethi@gmx.net>
2023-01-31 13:01:34 +0100

    Torn off LED.

--- kobo/init.sh
+++ kobo/init.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+### turn off front LED
+echo 0 > /sys/class/leds/GLED/brightness
+
 ### Note: the file below must NOT resist in scrips directory. Otherwise, we are not
 ###       able to Export USB storage!!
-/usr/set_brightness.sh & 
+file=/usr/set_brightness.sh
+if test -f "file"; then
+    $file &
+fi
+
