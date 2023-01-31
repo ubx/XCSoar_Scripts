71b2479c31f89f621ff4034a47f2bd5903c6b23c
Andreas Lüthi <andreas.luethi@gmx.net>
2023-01-31 10:25:19 +0100

    Support BT connection without wifi on.

--- kobo/scripts/sub/connect.sh
+++ kobo/scripts/sub/connect.sh
@@ -13,7 +13,10 @@ model=`dd if=/dev/mmcblk0 bs=8 count=1 skip=64`
 if [ `expr substr $model 1 7` = SN-N418 ] ; then # Libra 2
    /sbin/rtk_hciattach -n -s 115200 ttymxc1 rtk_h5 &
 elif [ `expr substr $model 1 7` = SN-N506 ] ; then # Clara 2E
-  /sbin/hciattach -n ttymxc1 any 1500000 flow -t 20 &
+   insmod /drivers/mx6sll-ntx/wifi/mlan.ko
+   insmod /drivers/mx6sll-ntx/wifi/moal.ko mod_para=nxp/wifi_mod_para_sd8987.conf
+   insmod /drivers/mx6sll-ntx/wifi/sdio_wifi_pwr.ko
+   /sbin/hciattach -n ttymxc1 any 1500000 flow -t 20 &
 fi
 
 hciconfig hci0 up
