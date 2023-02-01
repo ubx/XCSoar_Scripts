7835d96d4c047613c41bc0d4b040bb3285bfb8a2
Andreas Lüthi <andreas.luethi@gmx.net>
2023-02-01 10:07:00 +0100

    Typo.

--- kobo/init.sh
+++ kobo/init.sh
@@ -6,7 +6,7 @@ echo 0 > /sys/class/leds/GLED/brightness
 ### Note: the file below must NOT resist in scrips directory. Otherwise, we are not
 ###       able to Export USB storage!!
 file=/usr/set_brightness.sh
-if test -f "file"; then
+if test -f "$file"; then
     $file &
 fi
 
